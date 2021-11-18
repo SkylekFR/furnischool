ddebee43a679206f94fbfccd52dc188e2a080a44
Emilien C <chazpion2@gmail.com>
2021-11-18 15:13:38 +0100

    imported cloud firestore

--- app/src/main/java/com/emilien/champion/furnischool/MainActivity.kt
+++ app/src/main/java/com/emilien/champion/furnischool/MainActivity.kt
@@ -2,10 +2,27 @@ package com.emilien.champion.furnischool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emilien.champion.furnischool.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+import com.google.firestore.v1.Document
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.mainAddNewFurnitureButton.setOnClickListener {
+            val hashMap: MutableMap<String, String> = HashMap<String, String>()
+            hashMap["test"] = "test"
+            FirebaseFirestore.getInstance().collection("test").add(hashMap)
+        }
+
+
     }
 }
